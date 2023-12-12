e70dab005323930e60dca180305048bbaa63fc9e
enrik-rucaj <43344630+enrik-rucaj@users.noreply.github.com>
2023-12-12 09:45:23 +0100

    creati backend e frontend

---
+++ GestioneDipendenti-Backend/src/main/java/com/veggie/team/GestioneDipendentiBackend/GestioneDipendentiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veggie.team.GestioneDipendentiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneDipendentiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneDipendentiBackendApplication.class, args);
+	}
+
+}
---
+++ GestioneDipendenti-Backend/src/test/java/com/veggie/team/GestioneDipendentiBackend/GestioneDipendentiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veggie.team.GestioneDipendentiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneDipendentiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
